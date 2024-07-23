5a1d2535145122d91aca986efa7f47a8692194c9
boonflies <boonflies@users.noreply.github.com>
2024-07-23 16:10:44 +0530

    Add files via upload

---
+++ src/SG2RRT.cpp
@@ -0,0 +1,301 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Ioan Sucan */
+
+#include "ompl/geometric/planners/rrt/SG2RRT.h"
+#include <limits>
+#include "ompl/base/goals/GoalSampleableRegion.h"
+#include "ompl/tools/config/SelfConfig.h"
+
+ompl::geometric::SG2RRT::SG2RRT(const base::SpaceInformationPtr &si, bool addIntermediateStates)
+  : base::Planner(si, addIntermediateStates ? "RRTintermediate" : "SG2RRT")
+{
+    specs_.approximateSolutions = true;
+    specs_.directed = true;
+
+    Planner::declareParam<double>("range", this, &SG2RRT::setRange, &SG2RRT::getRange, "0.:1.:10000.");
+    Planner::declareParam<double>("goal_bias", this, &SG2RRT::setGoalBias, &SG2RRT::getGoalBias, "0.:.05:1.");
+    Planner::declareParam<bool>("intermediate_states", this, &SG2RRT::setIntermediateStates, &SG2RRT::getIntermediateStates,
+                                "0,1");
+
+    addIntermediateStates_ = addIntermediateStates;
+}
+
+ompl::geometric::SG2RRT::~SG2RRT()
+{
+    freeMemory();
+}
+
+void ompl::geometric::SG2RRT::clear()
+{
+    Planner::clear();
+    sampler_.reset();
+    freeMemory();
+    if (nn_)
+        nn_->clear();
+    lastGoalMotion_ = nullptr;
+}
+
+void ompl::geometric::SG2RRT::setup()
+{
+    Planner::setup();
+    tools::SelfConfig sc(si_, getName());
+    sc.configurePlannerRange(maxDistance_);
+
+    if (!nn_)
+        nn_.reset(tools::SelfConfig::getDefaultNearestNeighbors<Motion *>(this));
+    nn_->setDistanceFunction([this](const Motion *a, const Motion *b) { return distanceFunction(a, b); });
+}
+
+void ompl::geometric::SG2RRT::freeMemory()
+{
+    if (nn_)
+    {
+        std::vector<Motion *> motions;
+        nn_->list(motions);
+        for (auto &motion : motions)
+        {
+            if (motion->state != nullptr)
+                si_->freeState(motion->state);
+            delete motion;
+        }
+    }
+}
+
+ompl::base::PlannerStatus ompl::geometric::SG2RRT::solve(const base::PlannerTerminationCondition &ptc)
+{
+    checkValidity();
+    //const base::State *start = pis_.nextStart();
+    base::Goal *goal = pdef_->getGoal().get();
+    auto *goal_s = dynamic_cast<base::GoalSampleableRegion *>(goal);
+
+    // Check if there are any valid start states
+    const base::State *startState = pis_.nextStart();
+    if (startState == nullptr)
+    {
+      OMPL_ERROR("%s: There are no valid initial states!", getName().c_str());
+      return base::PlannerStatus::INVALID_START;
+    }
+
+    // Add all start states to the nearest-neighbor data structure
+    if (startState != nullptr)
+    {
+		
+        auto *motion = new Motion(si_);
+        si_->copyState(motion->state, startState);
+        nn_->add(motion);
+    }
+
+    if (nn_->size() == 0)
+    {
+        OMPL_ERROR("%s: There are no valid initial states!", getName().c_str());
+        return base::PlannerStatus::INVALID_START;
+    }
+
+    if (!sampler_)
+        sampler_ = si_->allocStateSampler();
+
+    OMPL_INFORM("%s: Starting planning with %u states already in datastructure", getName().c_str(), nn_->size());
+
+    Motion *solution = nullptr;
+    Motion *approxsol = nullptr;
+    double approxdif = std::numeric_limits<double>::infinity();
+    auto *rmotion = new Motion(si_);
+    base::State *rstate = rmotion->state;
+    base::State *xstate = si_->allocState();
+
+    // Allocate memory for two states
+    base::State *sampledStates[2];
+    for (int i = 0; i < 2; ++i)
+    {
+        sampledStates[i] = si_->allocState();
+    }
+
+    while (!ptc)
+    {
+        // Sample random state (with goal biasing)
+        if ((goal_s != nullptr) && rng_.uniform01() < goalBias_ && goal_s->canSample())
+            goal_s->sampleGoal(rstate);
+        else
+        {
+	    // Sample two random states
+           for (int i = 0; i < 2; ++i)
+           {
+              sampler_->sampleUniform(sampledStates[i]);
+           }
+
+       
+	 int bestIndex = 0;
+        double bestCombinedDist = std::numeric_limits<double>::infinity();
+
+        for (int i = 0; i < 2; ++i) {
+            double goalDist;
+            goal->isSatisfied(sampledStates[i], &goalDist);
+            
+            double startDist = si_->distance(startState, sampledStates[i]);
+            double combinedDist = startDist + goalDist;
+            
+            if (combinedDist < bestCombinedDist) {
+                bestCombinedDist = combinedDist;
+                bestIndex = i;
+            }
+        }	
+	
+
+        // Copy the best state to rstate
+        si_->copyState(rstate, sampledStates[bestIndex]);
+} 
+        // Find closest state in the tree
+        Motion *nmotion = nn_->nearest(rmotion);
+        base::State *dstate = rstate;
+
+        // Find state to add
+        double d = si_->distance(nmotion->state, rstate);
+        if (d > maxDistance_)
+        {
+            si_->getStateSpace()->interpolate(nmotion->state, rstate, maxDistance_ / d, xstate);
+            dstate = xstate;
+        }
+
+        if (si_->checkMotion(nmotion->state, dstate))
+        {
+            if (addIntermediateStates_)
+            {
+                std::vector<base::State *> states;
+                const unsigned int count = si_->getStateSpace()->validSegmentCount(nmotion->state, dstate);
+
+                if (si_->getMotionStates(nmotion->state, dstate, states, count, true, true))
+                    si_->freeState(states[0]);
+
+                for (std::size_t i = 1; i < states.size(); ++i)
+                {
+                    auto *motion = new Motion;
+                    motion->state = states[i];
+                    motion->parent = nmotion;
+                    nn_->add(motion);
+
+                    nmotion = motion;
+                }
+            }
+            else
+            {
+                auto *motion = new Motion(si_);
+                si_->copyState(motion->state, dstate);
+                motion->parent = nmotion;
+                nn_->add(motion);
+
+                nmotion = motion;
+            }
+
+            double dist = 0.0;
+            bool sat = goal->isSatisfied(nmotion->state, &dist);
+            if (sat)
+            {
+                approxdif = dist;
+                solution = nmotion;
+                break;
+            }
+            if (dist < approxdif)
+            {
+                approxdif = dist;
+                approxsol = nmotion;
+            }
+        }
+    }
+
+    bool solved = false;
+    bool approximate = false;
+    if (solution == nullptr)
+    {
+        solution = approxsol;
+        approximate = true;
+    }
+
+    if (solution != nullptr)
+    {
+        lastGoalMotion_ = solution;
+
+        // Construct the solution path
+        std::vector<Motion *> mpath;
+        while (solution != nullptr)
+        {
+            mpath.push_back(solution);
+            solution = solution->parent;
+        }
+
+        // Set the solution path
+        auto path(std::make_shared<PathGeometric>(si_));
+        for (int i = mpath.size() - 1; i >= 0; --i)
+            path->append(mpath[i]->state);
+        pdef_->addSolutionPath(path, approximate, approxdif, getName());
+        solved = true;
+    }
+
+    si_->freeState(xstate);
+    if (rmotion->state != nullptr)
+        si_->freeState(rmotion->state);
+    delete rmotion;
+
+    // Free the allocated sampled states
+    for (int i = 0; i < 2; ++i)
+    {
+        si_->freeState(sampledStates[i]);
+    }
+
+    OMPL_INFORM("%s: Created %u states", getName().c_str(), nn_->size());
+
+    return {solved, approximate};
+}
+
+
+void ompl::geometric::SG2RRT::getPlannerData(base::PlannerData &data) const
+{
+    Planner::getPlannerData(data);
+
+    std::vector<Motion *> motions;
+    if (nn_)
+        nn_->list(motions);
+
+    if (lastGoalMotion_ != nullptr)
+        data.addGoalVertex(base::PlannerDataVertex(lastGoalMotion_->state));
+
+    for (auto &motion : motions)
+    {
+        if (motion->parent == nullptr)
+            data.addStartVertex(base::PlannerDataVertex(motion->state));
+        else
+            data.addEdge(base::PlannerDataVertex(motion->parent->state), base::PlannerDataVertex(motion->state));
+    }
+}
